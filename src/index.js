330f24509c040a72ada54b01555e89db8c143095
dmitriy.budakov <dmitry.budakov@forforce.com>
2023-10-01 23:45:34 +0300

    Add questions and reviews

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './index.html';
 import './index.scss';
+import "bootstrap";
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    effect: "fade",
+    modules: [Navigation],
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
