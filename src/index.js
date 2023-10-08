f1fa548749c6f062028f8115f595c5eb7639b4c6
dmitriy.budakov <dmitry.budakov@forforce.com>
2023-10-08 16:26:57 +0300

    Update styles for mobile

--- src/index.js
+++ src/index.js
@@ -6,4 +6,17 @@ import { changeLanguage } from './js/translation'
 
 document.addEventListener('DOMContentLoaded', () => {
     changeLanguage();
+    //
+    // const burgerMenu = document.querySelector('.js--burger_menu')
+    //
+    // burgerMenu.classList.remove('hided')
+    //
+    // document.addEventListener("scroll", (event) => {
+    //     burgerMenu.classList.add('hided');
+    //     console.log('scroll')
+    // });
+    // document.addEventListener("scrollend", (event) => {
+    //     console.log('scrollend')
+    //     burgerMenu.classList.remove('hided')
+    // });
 })
